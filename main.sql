de9a7426c30809f8ade036c39e7d2e48e6e5806e
Vasil Hristov <72384597+vhristov03@users.noreply.github.com>
2021-07-08 17:38:14 +0300

    Removed inserted rows

--- main.sql
+++ main.sql
@@ -36,13 +36,7 @@ CREATE TABLE `job_listing` (
   `url` char(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `job_listing`
---
 
-INSERT INTO `job_listing` (`id`, `title`, `description`, `salary`, `company`, `url`) VALUES
-(16, 'aaaaa', 'qweqweqweqweqewqwe', 123123, '123123', 'https://www.google.com/'),
-(17, 'Kosachccccc', 'qweqeqweqwe', 111, 'Google', 'https://www.google.com/');
 
 -- --------------------------------------------------------
 
@@ -72,18 +66,7 @@ CREATE TABLE `user` (
   `email` char(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `user`
---
 
-INSERT INTO `user` (`id`, `username`, `password_hash`, `email`) VALUES
-(4, 'Test2', 'fd61a03af4f77d870fc21e05e7e80678095c92d808cfb3b5c279ee04c74aca13', 'test1@test1.test1'),
-(5, 'testing', '5fd924625f6ab16a19cc9807c7c506ae1813490e4ba675f843d5a10e0baacdb8', 'asdasd'),
-(7, '1111', '318aee3fed8c9d040d35a7fc1fa776fb31303833aa2de885354ddf3d44d8fb69', '2222'),
-(9, 'qqq', '7c2ecd07f155648431e0f94b89247d713c5786e1e73e953f2fe7eca39534cd6d', 'www'),
-(10, 'qkooo', 'a0ec06301bf1814970a70f89d1d373afdff9a36d1ba6675fc02f8a975f4efaeb', 'awewaeawe'),
-(11, '111122222', '183705900b3f56bd7b54652143c3dd52ce894996f737a45fab7a5ebf70b76dd1', '33333444'),
-(12, 'login', '428821350e9691491f616b754cd8315fb86d797ab35d843479e732ef90665324', 'login');
 
 --
 -- Indexes for dumped tables
